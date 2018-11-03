ed2160c9c5972c4e996f19edc2e71933f6f0daf6
Fenny <08jadez@gmail.com>
2018-11-03 16:28:02 +0100

    Update chromecastjs.js

--- chromecastjs.js
+++ chromecastjs.js
@@ -1,4 +1,5 @@
 var ChromecastJS = function(scope, reciever) {
+	
 	// Load framework if not exist
 	if (typeof cast === 'undefined') {
         var castFramework = document.createElement('script');
