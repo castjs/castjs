0fd35ba3336269588e087ed58951f669b77b3031
Fenny <08jadez@gmail.com>
2018-11-03 17:35:08 +0100

    Update chromecastjs.js

--- chromecastjs.js
+++ chromecastjs.js
@@ -1,6 +1,6 @@
 var ChromecastJS = function (scope, reciever) {
     // Load framework if not exist
-    if (typeof cast === 'undefined') {
+    if (typeof window.cast === 'undefined') {
         var castFramework = document.createElement('script');
         castFramework.src = 'https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1'
         document.body.appendChild(castFramework)
